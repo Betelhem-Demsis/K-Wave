6cc322740a2db902ca3606eb786097901ee55748
betelhem demsis <bettydemsis@gmail.com>
2024-06-21 03:14:16 +0300

    initial files

---
+++ sections/About.jsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      varients={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerwidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About MetaVerse" textStyles="text-center" />
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-center text-secondary-white"
+      >
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum laboriosam nemo dignissimos vero quas iure exercitationem reiciendis. Quidem optio dolorem iste illum facere officiis voluptate excepturi sint dolore voluptas beatae nulla laborum, nobis et facilis error, voluptatem consequuntur inventore consectetur labore nesciunt. Molestias unde repellendus tempore doloribus, iusto explicabo nisi.
+      </motion.p>
+      <motion.img variants={fadeIn('up', 'tween', 0.3, 1)} src="/arrow-down.svg" alt="arrow-down" className="w-[18px] h-[28px] object-contain mt-[28px]" />
+    </motion.div>
+  </section>
+);
+
+export default About;
