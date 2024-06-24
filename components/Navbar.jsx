862fe70da391cca9102884b56cdb77ec77d68374
betelhem demsis <bettydemsis@gmail.com>
2024-06-24 21:46:02 +0300

    modified

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,14 +14,13 @@ const Navbar = () => (
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-      {/* <img src="/search.svg" alt="search" className="w-[24px] h-[24px] object-contain" /> */}
       <h2 className="font-extrabold text-[24px] leading-[30px] text-slate-500">
         MULTIVERSE
       </h2>
       <img
         src="/menu.svg"
         alt="menu"
-        className="w-[24px] h-[24px] object-contain"
+        className="w-[24px] h-[24px] object-contain cursor-pointer"
       />
     </div>
   </motion.nav>
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import { motion } from 'framer-motion';
 import styles from '../styles';
-import { fadeIn, staggerContainer } from '../utils/motion';
+import { staggerContainer } from '../utils/motion';
 import { ExploreCard, TitleText, TypingText } from '../components';
 import { exploreWorlds } from '../constants';
 
@@ -22,15 +22,13 @@ const Explore = () => {
         <TypingText title="|The World" textStyles="text-center" />
         <TitleText title={(
           <>
-            Choose the world you want
-            <br className="md:block hidden" />
-            to explore
+            Choose the world you want <br className="md:block hidden" /> to explore
           </>
         )} textStyles="text-center"
         />
         <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
           {exploreWorlds.map((world, index) => (
-            <ExploreCard key={world.id} {...world} index={index} active={active} handleCLick={setActive} />
+            <ExploreCard key={world.id} {...world} index={index} active={active} handleClick={setActive} />
           ))}
         </div>
       </motion.div>
