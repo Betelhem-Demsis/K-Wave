6d2dd4094d8bbc13aa150e4c5d8546f9c6808c09
betelhem demsis <bettydemsis@gmail.com>
2024-06-26 19:16:24 +0300

    modified

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -7,7 +7,7 @@ import { fadeIn } from '../utils/motion';
 const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
   <motion.div
     variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
-    className={`relative ${active === id ? 'lg:flex-[2] flex-[]' : 'lg:flex-[0.5] flex-[1]'} flex item-center justify-cente min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+    className={`relative ${active === id ? 'lg:flex-[2] flex-[]' : 'lg:flex-[0.5] flex-[1]'} flex item-center justify-cente min-w-[150px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
     onClick={() => handleClick(id)}
   >
     <img
@@ -17,7 +17,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
     />
 
     <div className="absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
-      <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20">{title}</h3>
+      <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-5">{title}</h3>
     </div>
 
   </motion.div>
