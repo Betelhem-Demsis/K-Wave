47ea531af2065cd445f7b3c046b047dca1de73e3
betelhem demsis <bettydemsis@gmail.com>
2024-06-26 19:23:53 +0300

    modified

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,13 +1,12 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import styles from '../styles';
 import { fadeIn } from '../utils/motion';
 
 const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
   <motion.div
     variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
-    className={`relative ${active === id ? 'lg:flex-[2] flex-[]' : 'lg:flex-[0.5] flex-[1]'} flex item-center justify-cente min-w-[150px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+    className={`relative ${active === id ? 'lg:flex-[2] flex-[3]' : 'lg:flex-[0.5] flex-[1]'} flex item-center justify-cente min-w-[100px] h-[500px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
     onClick={() => handleClick(id)}
   >
     <img
